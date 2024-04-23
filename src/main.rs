ee5951a3f259cd00044732561d7b847cd9b06bc5
Yatsu <yatsu.dev@gmail.com>
2024-04-24 00:29:02 +0200

    aspen code moved to a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use dialoguer::{theme::ColorfulTheme, Select};
 use logos::Logos;
+use std::fs;
 mod lexer;
 
 fn main() {
-    let content = std::fs::read_to_string("fn.aspen").unwrap();
+    let names: Vec<_> = fs::read_dir("./aspen/")
+        .unwrap()
+        .map(|entry| entry.unwrap().file_name().to_string_lossy().to_string())
+        .collect();
+
+    let choice = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Which file to read?")
+        .items(&names)
+        .interact()
+        .unwrap();
+
+    let content = fs::read_to_string(&format!("./aspen/{}", names[choice])).unwrap();
     let mut lex = lexer::Token::lexer(&content);
 
     while let Some(token) = lex.next() {
