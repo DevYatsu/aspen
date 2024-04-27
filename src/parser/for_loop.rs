f0279d400a24fbf661ed063d9a09b118fd30de12
Yatsu <yatsu.dev@gmail.com>
2024-04-28 00:22:29 +0200

    for/while loops support add + several variables can now be declared with commas

---
+++ src/parser/for_loop.rs
@@ -0,0 +1,68 @@
+use super::{
+    error::{AspenError, AspenResult},
+    parse_block,
+    utils::{expect_space, next_jump_multispace, Block},
+    Expr, Statement,
+};
+use crate::parser::{AspenParser, Token};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct For<'a> {
+    pub args: Vec<&'a str>,
+    pub indexed: Expr<'a>,
+    pub body: Block<'a>,
+}
+
+impl<'s> For<'s> {
+    /// Parses a for loop.
+    ///
+    /// **NOTE: We assume "for" is already consumed by the parser!**
+    pub fn parse(parser: &mut AspenParser<'s>) -> AspenResult<Statement<'s>> {
+        expect_space(parser)?;
+        let args = Self::parse_args(parser)?;
+        expect_space(parser)?;
+
+        let indexed = Expr::parse(parser)?;
+
+        let token = next_jump_multispace(parser)?;
+        match token {
+            Token::OpenBrace => (),
+            _ => return Err(AspenError::Expected("a '{'".to_owned())),
+        };
+
+        let body = parse_block(parser, Some(Token::CloseBrace))?;
+
+        Ok(For {
+            args,
+            indexed,
+            body,
+        }
+        .into())
+    }
+
+    /// Parses the arguments of a for loop.
+    ///
+    /// **NOTE: This function also parses the following "->" Token!**
+    fn parse_args(parser: &mut AspenParser<'s>) -> AspenResult<Vec<&'s str>> {
+        let mut args = vec![];
+        let mut awaits_arg = true;
+
+        loop {
+            let token = next_jump_multispace(parser)?;
+
+            match token {
+                Token::Identifier(value) if awaits_arg => {
+                    args.push(value);
+                    awaits_arg = false
+                }
+                Token::In => break,
+                Token::Comma if !awaits_arg => awaits_arg = true,
+                _ => return Err(AspenError::Expected("a valid argument or '->'".to_owned())),
+            };
+        }
+
+        Ok(args)
+    }
+}
+
+crate::impl_from_for!(For, Statement);
---
+++ src/parser/while_loop.rs
@@ -0,0 +1,35 @@
+use super::{
+    error::{AspenError, AspenResult},
+    parse_block,
+    utils::{expect_space, next_jump_multispace, Block},
+    Expr, Statement,
+};
+use crate::parser::{AspenParser, Token};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct While<'a> {
+    pub condition: Expr<'a>,
+    pub body: Block<'a>,
+}
+
+impl<'s> While<'s> {
+    /// Parses a while loop.
+    ///
+    /// **NOTE: We assume "while" is already consumed by the parser!**
+    pub fn parse(parser: &mut AspenParser<'s>) -> AspenResult<Statement<'s>> {
+        expect_space(parser)?;
+        let condition = Expr::parse(parser)?;
+
+        let token = next_jump_multispace(parser)?;
+        match token {
+            Token::OpenBrace => (),
+            _ => return Err(AspenError::Expected("a '{'".to_owned())),
+        };
+
+        let body = parse_block(parser, Some(Token::CloseBrace))?;
+
+        Ok(While { condition, body }.into())
+    }
+}
+
+crate::impl_from_for!(While, Statement);
